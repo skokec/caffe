18f0fc7d2fb43f499d16804d3742d9e9c7e5381a
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2017-03-31 10:00:47 +0000

    Added "$@" to imagenet and cifar10 examples scripts

--- examples/cifar10/train_full_sigmoid.sh
+++ examples/cifar10/train_full_sigmoid.sh
@@ -3,5 +3,5 @@
 TOOLS=./build/tools
 
 $TOOLS/caffe train \
-    --solver=examples/cifar10/cifar10_full_sigmoid_solver.prototxt
+    --solver=examples/cifar10/cifar10_full_sigmoid_solver.prototxt $@
 
