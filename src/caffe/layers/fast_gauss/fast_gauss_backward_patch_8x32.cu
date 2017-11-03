94611e5ac36eba088abe1ef209d70d7008d59510
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2017-11-03 14:02:12 +0000

    Added missing files for patch_size_w=8 update

---
+++ src/caffe/layers/fast_gauss/fast_gauss_backward_patch_8x32.cu
@@ -0,0 +1,10 @@
+#include "caffe/layers/fast_gauss/fast_gauss_backward_core.hpp"
+
+namespace  caffe {
+
+void fast_gauss_backward_multi_subfeatures_patch_8x32(int IMG_PATCH_SIZE_W, int IMG_PATCH_SIZE_H, int MAX_OFFSET, bool SMALLER_WARP_AND_GROUP_K, int BATCH_IMAGES, bool USE_INTERPOLATION, bool SINGLE_SUBFEATURE, FastGaussBackward<float>::CUDAParams& PARAMS){
+
+    RUN_KERNEL_R5(FastGaussBackwardCUDA, 8, 32, MAX_OFFSET, SMALLER_WARP_AND_GROUP_K, BATCH_IMAGES, USE_INTERPOLATION, SINGLE_SUBFEATURE, PARAMS);
+}
+
+}
